a6973572fdbbc0af5fcef8e1d1af11242447abef
ArseBro228 <iaseshviliarsen@gmail.com>
2022-01-14 22:15:34 +0200

    Lesson29_hw

---
+++ main.js
@@ -0,0 +1,34 @@
+// let userName = "Arsen"
+// let adminName = "Verona"
+// let a = 5; b = 6; c = "Arsen"
+// alert(userName + " " + adminName + " " + (a+b))
+// alert("Hello, " + userName + ", давно не виделесь.")
+const arr = [5, 6, 8, "Arsen"]
+arr.forEach(i => console.log(i))
+
+let userName = prompt
+("What's your name?")
+alert("Welcome to site " + userName + "! ")
+let userAge = prompt
+("What is your age?")
+alert("Ok, you are " + userAge + " years old!")
+let userSex = prompt
+("What is your sex?(Male|Female)")
+alert("ok, you are " + userSex)
+let userEmail = prompt
+("what is your email?")
+alert("Your email is " + userEmail)
+let userPassword = prompt
+("last question - what is your password?")
+alert("thanks you for registration!")
+
+
+
+
+
+
+
+
+
+
+
